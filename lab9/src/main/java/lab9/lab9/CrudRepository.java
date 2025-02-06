111092bb5daac152f69b4a1de148439d780da96f
Maria-VP <160485272+Maria-VP@users.noreply.github.com>
2025-02-06 16:00:14 +0200

    Delete lab9 directory

--- lab9/src/main/java/lab9/lab9/CrudRepository.java
+++
@@ -1,4 +0,0 @@
-package lab9.lab9;
-
-public interface CrudRepository<Masina, T> {
-}
--- lab9/src/main/java/lab9/lab9/MasinaRepository.java
+++
@@ -1,9 +0,0 @@
-package lab9.lab9;
-
-import java.util.List;
-
-public interface MasinaRepository<Masina> extends CrudRepository<Masina, String> {
-    List<Masina> findByMarca(String marca);
-    List<Masina> findByKmLessThan(int km);
-    List<Masina> findByAnulFabricatieiGreaterThan(int anul);
-}
